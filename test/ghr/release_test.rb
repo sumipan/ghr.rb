71b81b43182f7dc39b32d923ce9c9c012bbdf051
takashi nagayasu <regist@g-onion.org>
2015-12-12 13:30:55 +0900

    Fix

--- test/ghr/release_test.rb
+++ test/ghr/release_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class Github::ReleaseTest < Minitest::Test
+class GHR::CommandsTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Github::Release::VERSION
+    refute_nil ::GHR::Commands::VERSION
   end
 
   def test_it_does_something_useful
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'github/release'
+require 'ghr/commands'
 
 require 'minitest/autorun'
