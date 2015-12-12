14a2afd0c9c23185ea840a14b814fefd7bf096db
takashi nagayasu <regist@g-onion.org>
2015-12-12 15:37:19 +0900

    Add

--- lib/ghr.rb
+++ lib/ghr.rb
@@ -4,9 +4,13 @@ module GHR
   module Commands
     # Your code goes here...
   end
+  autoload :Helper, 'ghr/helper'
 
   class << self
     def execute args
+      raise "You need to run this command from the toplevel of the working tree." unless Helper.root?
+      raise "You need to run `git flow init` before operation." unless Helper.inited?
+
     end
   end
 end
---
+++ lib/ghr/helper.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+module GHR
+  module Helper
+    
+    class << self
+
+      def inited?
+        return true
+      end
+      
+      def root?
+        return false
+      end
+    end
+    
+  end
+end
