2014376783b648b766082bf97f870a684a7dc37b
takashi nagayasu <regist@g-onion.org>
2015-12-12 23:37:29 +0900

    feature

--- lib/ghr/feature.rb
+++ lib/ghr/feature.rb
@@ -1,8 +1,47 @@
 # encoding: utf-8
 
 module GHR
-module Commands
   module Feature
+    class << self
+      def execute args
+        subcommand = args.shift
+        name = args.shift
+        
+        raise "name must specified." unless name.empty?
+        
+        case subcommand
+        when "start"
+          start name
+        when "finish"
+          finish name
+        else
+          help
+        end
+      end
+
+      def start name, options = {}
+        branch = GHR::Helper.branch "feature", name
+        GHR::Helper.exec("git flow feature start -F #{name}", true)
+      end
+      
+      def finish name, options = {}
+        branch = GHR::Helper.branch "feature", name
+
+        GHR::Helper.exec("git flow feature finish -F -m 'feature:finish' #{name}")
+      end
+      
+      def help
+        help = <<HELP
+usage: ghr release <subcommand> version
+
+Available subcommands are:
+   start   release start
+   freeze  release freeze (non git-flow operation)
+   finish  release finish
+HELP
+
+        puts help
+      end
+    end
   end
-end
 end
