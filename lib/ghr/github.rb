1337fa506b37c22835afd5b254de5200b4631875
takashi nagayasu <nagayasu@kayac.com>
2015-12-14 00:03:27 +0900

    Update

--- lib/ghr/github.rb
+++ lib/ghr/github.rb
@@ -7,10 +7,35 @@ module GHR
 
     module PullRequests
       class << self
-        def create base, head, title, body = nil
+        def create base, head, title, body = ""
+          pull_request = GHR.client.pull_requests.create({
+            :base  => base,
+            :head  => head,
+            :title => title,
+            :body  => body,
+          }).body
+
+          pull_request
+        end
+
+        def mergeable? base, head
+          pull_request = find base, head
+          raise "no pull_request found." unless pull_request
+
+          pull_request.mergeable == true
         end
 
         def find base, head
+          pull_requests = GHR.client.pull_requests.list({
+            :base  => base,
+            :head  => head,
+          }).body
+
+          if pull_requests && pull_requests.size > 0 then
+            return GHR.client.pull_requests.get({ :number => pull_requests.first.number }).body
+          else
+            return nil
+          end
         end
       end
     end
