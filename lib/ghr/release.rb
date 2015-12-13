f99e46110779e9cf9c8de41628534d0a647cd482
takashi nagayasu <nagayasu@kayac.com>
2015-12-13 16:00:55 +0900

    push

--- lib/ghr/release.rb
+++ lib/ghr/release.rb
@@ -6,17 +6,19 @@ module GHR
       def execute args
         subcommand = args.shift
         version = args.shift
-        
+
         raise "version must be number." unless version.to_i > 0
 
         branch = GHR::Helper.branch "release", version
-        
+
         case subcommand
         when "start"
           start version
+          publish version
         when "publish"
           GHR::Helper.exec("git checkout -f #{branch}", true)
           publish version
+          GHR::Helper.exec("git push #{Helper.remotes.first} #{branch}:#{branch}", true)
         when "freeze"
           GHR::Helper.exec("git checkout -f #{branch}", true)
           freeze version
@@ -31,12 +33,12 @@ module GHR
       def start version, options = {}
         GHR::Helper.exec("git flow release start #{version}", true)
       end
-      
+
       # non git-flow operation
       # Code freeze needed
       def freeze version, options = {}
       end
-      
+
       def finish version, options = {}
         # -n no tag
         # -p push origin
@@ -47,11 +49,11 @@ module GHR
         # GHR::Helper.exec("git merge #{GHR::Helper.master}")
         # GHR::Helper.exec("git push #{GHR::Helper.remotes.first} #{GHR::Helper.develop}:#{GHR::Helper.develop}")
       end
-      
+
       def publish version, options = {}
         GHR::Helper.exec("git flow release publish #{version}", true)
       end
-      
+
       def help
         help = <<HELP
 usage: ghr release <subcommand> version
