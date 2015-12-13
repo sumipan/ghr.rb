1cfbaf0ff97132af896004fd591eac8670b10f1b
takashi nagayasu <nagayasu@kayac.com>
2015-12-13 15:58:47 +0900

    push

--- lib/ghr/feature.rb
+++ lib/ghr/feature.rb
@@ -6,20 +6,22 @@ module GHR
       def execute args
         subcommand = args.shift
         name = args.shift
-        
+
         raise "name must specified." if !name || name.empty?
 
         branch = GHR::Helper.branch "feature", name
-        
+
         case subcommand
         when "start"
           start name
+          publish name
         when "finish"
           GHR::Helper.exec("git checkout -f #{branch}", true)
           finish name
         when "publish"
           GHR::Helper.exec("git checkout -f #{branch}", true)
           publish name
+          GHR::Helper.exec("git push #{Helper.remotes.first} #{branch}:#{branch}", true)
         else
           help
         end
@@ -28,19 +30,18 @@ module GHR
       def start name, options = {}
         GHR::Helper.exec("git flow feature start #{name}", true)
       end
-      
+
       def finish name, options = {}
         branch = GHR::Helper.branch "feature", name
         GHR::Helper.exec("git flow feature finish #{name}", true)
       end
-      
+
       def help
         help = <<HELP
 usage: ghr feature <subcommand> name
 
 Available subcommands are:
    start   feature start
-   publish feature publish
    finish  feature finish
 HELP
 
